663a70f3d6fd26ef2e810fc2af6227f5b505f583
XiangPan <xiangpan@nyu.edu>
2022-02-16 15:47:30 -0500

    init

---
+++ make_submission.sh
@@ -0,0 +1,16 @@
+###
+ # @Author: Xiang Pan
+ # @Date: 2022-02-04 02:22:59
+ # @LastEditTime: 2022-02-04 02:23:20
+ # @LastEditors: Xiang Pan
+ # @Description: 
+ # @FilePath: /Assignment1/make_submission.sh
+ # @email: xiangpan@nyu.edu
+### 
+rm -r ./xp2030
+mkdir ./xp2030
+cp ./mlp.py ./xp2030/
+cp ./build/theory.pdf ./xp2030/
+zip -r ./xp2030.zip ./xp2030/
+
+
---
+++ mlp.py
@@ -0,0 +1,9 @@
+'''
+Author: Xiang Pan
+Date: 2022-02-04 02:22:20
+LastEditTime: 2022-02-04 02:22:21
+LastEditors: Xiang Pan
+Description: 
+FilePath: /NYU_DL/Assignment1/mlp.py
+@email: xiangpan@nyu.edu
+'''
